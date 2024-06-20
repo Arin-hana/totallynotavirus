e15e2625f4bc3dace5f523ee036d77399a63f286
Arin Hanabi <91265178+Arin-hana@users.noreply.github.com>
2024-06-20 17:41:42 +0700

    Add files via upload

--- text.ps1
+++ text.ps1
@@ -1,4 +1,13 @@
 netsh wlan show profiles | Out-file -FilePath "./output.txt"
-Write-Host "please fix output before continue"
-Read-Host -Prompt "Press any key after fix"
-foreach($ussd in Get-Content ./output.txt){ netsh wlan show profile "$ussd" key=clear | Out-file -filepath "./ussd.txt" -append}
+#Write-Host "please fix output before continue"
+#Read-Host -Prompt "Press any key after fix"
+$input = Get-content "./output.txt" -raw
+$pattern = "All User Profile\s*:\s*(.*)"
+$matches = [regex]::Matches($input, $pattern)
+
+if ($matches.Count -gt 0) {
+    foreach ($match in $matches) {
+        $profileName = $match.Groups[1].Value.Trim()
+        netsh wlan show profile "$profileName" key=clear | Out-file -filepath "./ussd.txt" -append
+    }
+}
